c380ee2f493a085b4e9c1dbfe96c64bc3b46e1cc
Bowen WU <bowenbhwu@gmail.com>
2021-09-10 22:32:38 +0800

    Updated: Terminal messages

--- resources/report_resource.py
+++ resources/report_resource.py
@@ -24,27 +24,26 @@ class ReportResource(Resource):
                         required=False
                         )
 
-    put_parser = reqparse.RequestParser()
-    put_parser.add_argument('template',
-                        type=str,
-                        required=True
-                        )
-    put_parser.add_argument('label',
-                        type=str,
-                        required=False
-                        )
-
     @excpetion_handler
     def post(self):
         data = ReportResource.post_parser.parse_args()
+        
+        ip = request.remote_addr
+        label = data['label'].strip()
+        message = data['message'].strip()
+        dt_str = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 
-        report = Report(label=data['label'], message=data['message'], ip=request.remote_addr)
+        report = Report(label=label, message=message, ip=ip)
         report.save()
+        
+        print(f"[{dt_str}] From {ip}: ")
+        print(f"Label: {label}")
+        print(message)
 
         if data['label'] is not None:
-            with open(f"./logs/{data['label']}.txt", 'ab') as file:
-                file.write(f"[{datetime.now().strftime('%Y/%m/%d %H:%M:%S')}] From {request.remote_addr}:\n".encode('UTF-8'))
-                file.write(data['message'].encode('UTF-8'))
+            with open(f"./logs/{label}.txt", 'ab') as file:
+                file.write(f"\n[{dt_str}] From {request.remote_addr}:\n".encode('UTF-8'))
+                file.write(message.encode('UTF-8'))
 
         return {'message': 'Success'}, 201
 
